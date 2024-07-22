4175df0f8d0a79ce8a8ede790d97cf9f0a1a7c4c
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2024-07-22 17:27:17 +0700

    Create restart_xray.sh

---
+++ restart_xray.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Tìm ID container của sing-box
+CONTAINER_ID=$(docker ps -q --filter "name=xray")
+
+# Kiểm tra xem container có đang chạy không
+if [ -n "$CONTAINER_ID" ]; then
+    echo "Restarting sing-box container..."
+    docker restart $CONTAINER_ID
+else
+    echo "sing-box container is not running."
+fi
+
+# Thêm cron job nếu chưa tồn tại
+(crontab -l | grep -q "restart_xray.sh") || (crontab -l; echo "0 */3 * * * $(realpath $0)") | crontab -
